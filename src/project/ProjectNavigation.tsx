5cc08395075164a5b6e7e91fa36326b9de6105aa
jlevine456 <jlevine456@users.noreply.github.com>
2026-08-13 08:42:02 -0400

    Restore project navigation module

---
+++ src/project/ProjectNavigation.tsx
@@ -0,0 +1,11 @@
+import type { ProjectView } from "./ProjectView";
+
+export const projectNavigationItems: ReadonlyArray<[ProjectView, string]> = [
+  ["overview", "Overview"],
+  ["intake", "Client Files"],
+  ["audioPrep", "Audio Prep"],
+  ["references", "References"],
+  ["revisions", "Revisions"],
+  ["delivery", "Delivery"],
+  ["files", "Files"],
+];
