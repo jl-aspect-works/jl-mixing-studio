b1d92a3544fccb1e734a89331cd10e5e57b8fde4
jlevine456 <jlevine456@users.noreply.github.com>
2026-08-14 21:48:06 -0400

    Reuse one overview file index

--- src/project/ProjectOverviewDetails.tsx
+++ src/project/ProjectOverviewDetails.tsx
@@ -1,5 +1,6 @@
 import type { ClientSummary, DerivedTask, ProjectSummary, RevisionSummary } from "../types";
 import type { IntakeReportState } from "../AppShellViews";
+import { useProjectOverviewFileIndex } from "./ProjectOverviewFileIndex";
 import { ProjectOverviewFileSystem } from "./ProjectOverviewFileSystem";
 import { ProjectOverviewHealth } from "./ProjectOverviewHealth";
 import { ProjectOverviewQuickActions } from "./ProjectOverviewQuickActions";
@@ -7,16 +8,18 @@ import { ProjectOverviewRecentRevisions } from "./ProjectOverviewRecentRevisions
 import { ProjectOverviewSummary } from "./ProjectOverviewSummary";
 
 export function ProjectOverviewDetails({ client, project, tasks, intakeReport, loading, revisionCreationAvailable, revisionApprovalAvailable, onNewRevision, onApproveRevision, onRevisions }: { client: ClientSummary; project: ProjectSummary; tasks: DerivedTask[]; intakeReport: IntakeReportState; loading: boolean; revisionCreationAvailable: boolean; revisionApprovalAvailable: boolean; onNewRevision: () => void; onApproveRevision: (revision: RevisionSummary) => void; onRevisions: () => void }) {
+  const fileIndex = useProjectOverviewFileIndex(client.clientId, project.projectId);
+
   return (
     <div className="overview-layout">
       <div className="overview-top-grid">
-        <ProjectOverviewSummary clientId={client.clientId} project={project} tasks={tasks} intakeReport={intakeReport} />
-        <ProjectOverviewHealth clientId={client.clientId} project={project} tasks={tasks} intakeReport={intakeReport} />
+        <ProjectOverviewSummary project={project} tasks={tasks} intakeReport={intakeReport} fileIndex={fileIndex} />
+        <ProjectOverviewHealth project={project} tasks={tasks} intakeReport={intakeReport} fileIndex={fileIndex} />
         <ProjectOverviewQuickActions client={client} project={project} loading={loading} revisionCreationAvailable={revisionCreationAvailable} revisionApprovalAvailable={revisionApprovalAvailable} onNewRevision={onNewRevision} onApproveRevision={onApproveRevision} onRevisions={onRevisions} />
       </div>
       <div className="overview-bottom-grid">
         <ProjectOverviewRecentRevisions project={project} onRevisions={onRevisions} />
-        <ProjectOverviewFileSystem clientId={client.clientId} projectId={project.projectId} />
+        <ProjectOverviewFileSystem fileIndex={fileIndex} />
       </div>
     </div>
   );
