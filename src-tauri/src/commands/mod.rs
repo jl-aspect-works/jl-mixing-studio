75f0dd424a3c1328c47967f898c861426419415c
jlevine456 <jlevine456@users.noreply.github.com>
2026-08-15 09:16:04 -0400

    Register revision description command

--- src-tauri/src/commands/mod.rs
+++ src-tauri/src/commands/mod.rs
@@ -12,6 +12,8 @@ mod project_files;
 mod project_references;
 #[path = "project_revision_file_command.rs"]
 mod project_revision_files;
+#[path = "revision_description_command.rs"]
+mod revision_description;
 #[path = "revision_notes_command.rs"]
 mod revision_notes;
 #[path = "system_command.rs"]
@@ -29,6 +31,7 @@ pub(super) use project_file_summary::summarize_project_files;
 pub(super) use project_files::{delete_project_file, list_project_files, rename_project_file};
 pub(super) use project_references::{add_project_reference, delete_project_reference};
 pub(super) use project_revision_files::{delete_revision_file, rename_revision_file};
+pub(super) use revision_description::update_revision_description;
 pub(super) use revision_notes::{get_revision_notes, update_revision_notes};
 pub(super) use system::{discover_default_workspace, get_jl_mixing_version, get_system_info};
 pub(super) use workspace_configuration::{
