f4b04071b8452f2480850ca23981b38d60b7c212
jlevine456 <jlevine456@users.noreply.github.com>
2026-08-16 15:15:15 -0400

    Tighten Files screen shell

--- src/project/ProjectFilesShellView.tsx
+++ src/project/ProjectFilesShellView.tsx
@@ -1,5 +1,4 @@
 import type { ClientSummary, ProjectSummary } from "../types";
-import { FolderControl } from "../AppShellViews";
 import { ProjectNavigationBar } from "./ProjectNavigationBar";
 import type { ProjectShellView } from "./ProjectView";
 import { ProjectFilesWorkspace } from "./files/ProjectFilesWorkspace";
@@ -7,13 +6,6 @@ import { ProjectFilesWorkspace } from "./files/ProjectFilesWorkspace";
 export function ProjectFilesShellView({ client, project, onSelectView }: { client: ClientSummary; project: ProjectSummary; onProjects: () => void; onOverview: () => void; onSelectView: (view: ProjectShellView) => void }) {
   return <>
     <ProjectNavigationBar active="files" onSelect={onSelectView} />
-    <section className="directory-toolbar">
-      <div>
-        <p className="kicker">{client.clientName}</p>
-        <h2>Files</h2>
-      </div>
-      <FolderControl location="project" clientId={client.clientId} projectId={project.projectId} />
-    </section>
     <ProjectFilesWorkspace clientId={client.clientId} projectId={project.projectId} />
   </>;
 }
