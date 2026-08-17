c8f7c06fc3fef13106efe3a79b41549419a72664
jlevine456 <jlevine456@users.noreply.github.com>
2026-08-16 21:08:08 -0400

    Load transient success feedback styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./WorkspaceSidebar.css";
 import "./ButtonHierarchy.css";
+import "./SuccessFeedback.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
