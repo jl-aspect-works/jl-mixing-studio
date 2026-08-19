6f306e5bc13c6b50f47d8ac026c2a33eddfe22e0
jlevine456 <jlevine456@users.noreply.github.com>
2026-08-19 06:49:47 -0400

    Apply shared focus treatment globally

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./WorkspaceSidebar.css";
 import "./ButtonHierarchy.css";
 import "./SuccessFeedback.css";
+import "./FocusTreatment.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
