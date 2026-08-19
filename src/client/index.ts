db006150b97c3e5040cc8e124509377411f8b0ab
jlevine456 <jlevine456@users.noreply.github.com>
2026-08-19 07:54:33 -0400

    Use projects-first Client Details route

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,3 +1,4 @@
 export { ClientDialog, type ClientDialogProps } from "./ClientDialog";
-export { ClientDetails, ClientsRoute } from "./ClientViews";
+export { ClientsRoute } from "./ClientViews";
+export { ClientDetails } from "./ClientDetailsV21";
 export { useClientWorkflow, type UseClientWorkflowOptions } from "./controller";
