6b6dfbb963b31f18858514c9eb5b1fa728a41f44
jlevine456 <jlevine456@users.noreply.github.com>
2026-08-21 19:17:02 -0400

    Apply global search placeholder removal styling

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./WorkspaceSidebar.css";
 import "./ButtonHierarchy.css";
 import "./SuccessFeedback.css";
 import "./FocusTreatment.css";
+import "./GlobalSearchRemoval.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
