4dc3113755de7778cc4cc51a80fa25812f650b82
jlevine456 <jlevine456@users.noreply.github.com>
2026-08-21 17:23:20 -0400

    Add filter action icon

--- src/components/ActionIcon.tsx
+++ src/components/ActionIcon.tsx
@@ -10,6 +10,7 @@ export type ActionIconName =
   | "copy"
   | "delete"
   | "edit"
+  | "filter"
   | "folder"
   | "import"
   | "open"
@@ -33,6 +34,7 @@ const iconPaths: Record<ActionIconName, ReactNode> = {
   copy: <><rect x="9" y="9" width="11" height="11" rx="2"/><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"/></>,
   delete: <><path d="M3 6h18"/><path d="M8 6V4h8v2"/><path d="m19 6-1 14H6L5 6"/><path d="M10 11v5M14 11v5"/></>,
   edit: <><path d="M12 20h9"/><path d="M16.5 3.5a2.1 2.1 0 0 1 3 3L8 18l-4 1 1-4Z"/></>,
+  filter: <><path d="M4 5h16"/><path d="M7 12h10"/><path d="M10 19h4"/></>,
   folder: <><path d="M3 6h7l2 2h9v11H3z"/><path d="M3 6V4h7l2 2"/></>,
   import: <><path d="M12 3v12"/><path d="m7 10 5 5 5-5"/><path d="M5 21h14"/></>,
   open: <><path d="M5 19 19 5"/><path d="M10 5h9v9"/></>,
